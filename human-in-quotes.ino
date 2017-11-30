a881d0a49567c91e5cf51da24495ed520656393e
Ben Yang <thisisbenbtw@gmail.com>
2017-11-30 12:15:42 -0800

    Stopped using PID
    simple control based on boolean readings

--- human-in-quotes.ino
+++ human-in-quotes.ino
@@ -9,8 +9,16 @@
 #define M125 10
 #define M120 11
 
-int rbias, mbias, lbias;
+#define ISLEFT -1
+#define ISCENTER 0
+#define ISRIGHT 1
+#define STOP 5
 
+int rbias, mbias, lbias;
+int sleft, sright, scenter;
+int state;
+int stopv = 0;
+int count = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -41,56 +49,96 @@ void setup() {
   //startMotors();
 }
 
-int prev_error = 0;
-int sum_error = 0;
-int KP = 50;
-int KI = 1;
-int KD = 25;
-
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(RED, LOW);
-  digitalWrite(GRN, LOW);
-  digitalWrite(BLU, LOW);
   
   int left = analogRead(LEFT_S)-lbias;
   int right = analogRead(RIGHT_S)-rbias;
   int mid = analogRead(MID_S)-mbias;
-//  Serial.print(left);
-//  Serial.print(" ");
-//  Serial.print(mid);
-//  Serial.print(" ");
-//  Serial.println(right);
-  
-  int error = left*2-right;
-  int P,I,D;
-  P = KP*error;
-  I = KI*sum_error;
-  D = KD*(error-prev_error);
-  int changespeed = (P+D)/50;
-  Serial.println(changespeed);
+  Serial.print(left);
+  Serial.print(" ");
+  Serial.print(mid);
+  Serial.print(" ");
+  Serial.println(right);
 
-  if(changespeed<-5){ // turn right
-    digitalWrite(RED, HIGH);
-    //changespeed = -15;
-  }else if(changespeed>5){ // turn left
-    digitalWrite(BLU, HIGH);
-    //changespeed = 15; // either bump up change or make it variable somehow
-  }else{ // go straight
-    digitalWrite(GRN, HIGH);
-    //changespeed = 0;
+  sleft = (left<-8);
+  sright = (right<-8);
+  scenter = (mid<50);
+  /*
+  if(sleft && sright && scenter){ // sees horizontal line
+    if(stopv == 1)
+      state = STOP;
+    else if(stopv == 0)
+      stopv = 1;
   }
-  //changespeed = 0;
-  analogWrite(M125,177+changespeed);
-  analogWrite(M120,75+changespeed);
+  else if(!scenter && state == ISCENTER){ // previously centered but got off line
+    if(sleft)
+      state = ISRIGHT;
+    if(sright)
+      state = ISLEFT;
+    if(sleft && sright)
+      state = ISCENTER;
+  }
+  else if(state != ISCENTER && sleft){ // turning and sees the line with left
+    state = ISRIGHT;
+  }
+  else if(state != ISCENTER && sright){ // turning and sees the line with right
+    state = ISLEFT;
+  }
+  else if(state != ISCENTER && scenter){ // sees the line again after being off
+    state = ISCENTER;
+  }
 
-  prev_error = error;
-  if(error<-3)
-    error /= 2;
-  if(error>2)
-    error *= 3;
-  
-  sum_error += error;
+  if(state == ISRIGHT){
+    digitalWrite(BLU, HIGH);
+    analogWrite(M125,177+20);
+    analogWrite(M120,75+20);
+  }
+  else if(state == ISLEFT){
+    digitalWrite(RED, HIGH); 
+    analogWrite(M125,177-20);
+    analogWrite(M120,75-20);
+  }
+  else if(state == ISCENTER){
+    digitalWrite(GRN, HIGH);
+    analogWrite(M125,177);
+    analogWrite(M120,75);
+  }
+  else if(state == STOP){
+    digitalWrite(RED, HIGH);
+    digitalWrite(BLU, HIGH);
+    analogWrite(M125,255);
+    analogWrite(M120,0);
+  }
+  count++;
+  if(count%15)
+    stopv = 0;
+    */
+
+  if(sleft && !sright){
+    //go left
+    digitalWrite(BLU, HIGH);
+    digitalWrite(RED, LOW);
+    digitalWrite(GRN, LOW);
+    analogWrite(M125,177+20);
+    analogWrite(M120,75+20);
+  }
+  if(!sleft && sright){
+    //go right
+    digitalWrite(RED, HIGH); 
+    digitalWrite(GRN, LOW);
+    digitalWrite(BLU, LOW);
+    analogWrite(M125,177-20);
+    analogWrite(M120,75-20);
+  }
+  if(!sleft && scenter && !sright){
+    //go straight
+    digitalWrite(GRN, HIGH);
+    digitalWrite(RED, LOW);
+    digitalWrite(BLU, LOW);
+    analogWrite(M125,177);
+    analogWrite(M120,75);
+  }
   delay(10);
 }
 
