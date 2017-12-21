cff01034c09ca472fa21b4cc62e9589d4463d30b
Ben Yang <thisisbenbtw@gmail.com>
2017-12-21 14:57:26 -0800

    cleaned up code

--- human-in-quotes.ino
+++ human-in-quotes.ino
@@ -9,19 +9,14 @@
 #define M125 10
 #define M120 11
 
-#define ISLEFT -1
-#define ISCENTER 0
-#define ISRIGHT 1
-
-#define NOSTOP 10
-#define LINEONE 11
-#define EMPTYSPACE 12
-#define STOP 13
+#define NOSTOP 0
+#define LINEONE 1
+#define EMPTYSPACE 2
+#define STOP 3
 
 int rbias, mbias, lbias;
 int sleft, sright, scenter;
 int stopstate = NOSTOP;
-int count = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -58,68 +53,25 @@ void loop() {
   int left = analogRead(LEFT_S)-lbias;
   int right = analogRead(RIGHT_S)-rbias;
   int mid = analogRead(MID_S)-mbias;
-  Serial.print(left);
-  Serial.print(" ");
-  Serial.print(right);
-  Serial.print(" ");
-  Serial.println(mid);
+//  Serial.print(left);
+//  Serial.print(" ");
+//  Serial.print(right);
+//  Serial.print(" ");
+//  Serial.println(mid);
 
   sleft = (left<-8);
   sright = (right<-8);
   scenter = (mid<50);
-  /*
-  if(sleft && sright && scenter){ // sees horizontal line
-    if(stopv == 1)
-      state = STOP;
-    else if(stopv == 0)
-      stopv = 1;
-  }
-  else if(!scenter && state == ISCENTER){ // previously centered but got off line
-    if(sleft)
-      state = ISRIGHT;
-    if(sright)
-      state = ISLEFT;
-    if(sleft && sright)
-      state = ISCENTER;
-  }
-  else if(state != ISCENTER && sleft){ // turning and sees the line with left
-    state = ISRIGHT;
-  }
-  else if(state != ISCENTER && sright){ // turning and sees the line with right
-    state = ISLEFT;
-  }
-  else if(state != ISCENTER && scenter){ // sees the line again after being off
-    state = ISCENTER;
-  }
-
-  if(state == ISRIGHT){
-    digitalWrite(BLU, HIGH);
-    analogWrite(M125,177+20);
-    analogWrite(M120,75+20);
-  }
-  else if(state == ISLEFT){
-    digitalWrite(RED, HIGH); 
-    analogWrite(M125,177-20);
-    analogWrite(M120,75-20);
-  }
-  else if(state == ISCENTER){
-    digitalWrite(GRN, HIGH);
-    analogWrite(M125,177);
-    analogWrite(M120,75);
-  }
-  else if(state == STOP){
-    digitalWrite(RED, HIGH);
-    digitalWrite(BLU, HIGH);
-    analogWrite(M125,255);
-    analogWrite(M120,0);
-  }
-  count++;
-  if(count%15)
-    stopv = 0;
-    */
+  
   if(stopstate == NOSTOP){
     if(sleft && scenter && sright){
       stopstate = LINEONE;
+      stopstate = STOP;
+      digitalWrite(RED, HIGH);
+      digitalWrite(BLU, HIGH);
+      analogWrite(M125,255);
+      analogWrite(M120,0);
+      delay(99999);
     }
   }else if(stopstate == LINEONE){
     if(!sleft || !scenter || !sright){
@@ -141,16 +93,16 @@ void loop() {
     digitalWrite(BLU, HIGH);
     digitalWrite(RED, LOW);
     digitalWrite(GRN, LOW);
-    analogWrite(M125,177+20);
-    analogWrite(M120,75+20);
+    analogWrite(M125,177+30);
+    analogWrite(M120,75+25);
   }
   if(!sleft && sright){
     //go right
     digitalWrite(RED, HIGH); 
     digitalWrite(GRN, LOW);
     digitalWrite(BLU, LOW);
-    analogWrite(M125,177-20);
-    analogWrite(M120,75-20);
+    analogWrite(M125,177-25);
+    analogWrite(M120,75-30);
   }
   if(!sleft && scenter && !sright){
     //go straight
@@ -160,12 +112,6 @@ void loop() {
     analogWrite(M125,177);
     analogWrite(M120,75);
   }
-  //delay(10);
 }
 
-void startMotors(){
-  analogWrite(M125,180);
-  analogWrite(M120,90);
-  delay(500);
-}
 
